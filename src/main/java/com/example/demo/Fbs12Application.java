ea04ff6d5ded5e76f510b407f6a44c6a8bff1633
RekhaMantena <rekhamantena8@gmail.com>
2024-11-09 11:54:53 +0530

    fc

---
+++ src/main/java/com/example/demo/Fbs12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fbs12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fbs12Application.class, args);
+	}
+
+}
